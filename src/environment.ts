739fdef32439ddb7806d45b9d8eaaafcfae73779
Rathik Pratap <52971518+rathikpratap@users.noreply.github.com>
2025-01-08 23:15:02 +0530

    root path changed

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export const appConfig = {
-    apiUrl : 'http://localhost:4000/creator',
-    // apiUrl : 'https://www.creator.admixmedia.in/api',
+    // apiUrl : 'http://localhost:4000/creator',
+    apiUrl : 'https://www.creator.admixmedia.in/creator',
     production: false
 }
